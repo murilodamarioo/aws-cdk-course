0138903d286bbb4d871545368ac2da62cebdb0cf
Murilo Damario <murilo.gcdamario@gmail.com>
2022-11-30 18:39:19 -0300

    Cria DynamoDB e da permissão´para lambda

--- bin/ecommerce-aws.ts
+++ bin/ecommerce-aws.ts
@@ -9,6 +9,7 @@ import { EcommerceApiStack } from '../lib';
 
 const app = new cdk.App();
 
+// Defining acount and region where application will be available
 const env: cdk.Environment = {
   account: '321404749782',
   region: 'us-east-2'
@@ -24,6 +25,7 @@ const productsAppStack = new ProductsAppStack(app, 'ProductsApp', {
   env: env
 })
 
+// Add stack dependence to ecommerceApiStack
 const ecommerceApiStack = new EcommerceApiStack(app, 'ECommerceApi', {
   productsFetchHandler: productsAppStack.productsFecthHandler,
   tags: tags,
--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -4,15 +4,33 @@ import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs'
 
 import * as cdk from 'aws-cdk-lib'
 
+import * as dynamoDb from 'aws-cdk-lib/aws-dynamodb'
+
 import { Construct } from 'constructs'
 
 export class ProductsAppStack extends cdk.Stack {
     readonly productsFecthHandler: lambdaNodeJS.NodejsFunction
+    readonly productsDynamoDb: dynamoDb.Table
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props)
+
+        // DynamoDB table creation
+        this.productsDynamoDb = new dynamoDb.Table(this, 'ProductsDynamoDb', {
+            tableName: 'Products',
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            partitionKey: {
+                name: 'id',
+                type: dynamoDb.AttributeType.STRING
+            },
+            billingMode: dynamoDb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity: 1
+        })
+
+        // Lambda creation
         this.productsFecthHandler = new lambdaNodeJS.NodejsFunction(this, 'ProductsFecthFunction', {
-            functionName: 'ProductsFecthFuntion',
+            functionName: 'ProductsFecthFunction',
             entry: 'lambda/products/productsFetchFunction.ts',
             runtime: lambda.Runtime.NODEJS_16_X,
             handler: 'handler',
@@ -21,7 +39,12 @@ export class ProductsAppStack extends cdk.Stack {
             bundling: {
                 minify: true,
                 sourceMap: false,
+            },
+            environment: {
+                PRODUCTS_DDB: this.productsDynamoDb.tableName
             }
         })
+
+        this.productsDynamoDb.grantReadData(this.productsFecthHandler)
     }  
 }
