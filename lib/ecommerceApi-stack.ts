a35b31ae14a52846f3a22a2d606d8441bf1e0ddd
Murilo Damario <murilo.gcdamario@gmail.com>
2022-11-30 00:22:03 -0300

    Integra API Gateway com a função de pesquisa de produtos

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -8,13 +8,39 @@ import * as apigateway from 'aws-cdk-lib/aws-apigateway'
 
 import * as cwlogs from 'aws-cdk-lib/aws-logs'
 
+interface EcommerceApiStackProps extends cdk.StackProps {
+    productFetchHandler: lambdaNodeJS.NodejsFunction
+}
+
 export class EcommerceApiStack extends cdk.Stack {
 
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props: EcommerceApiStackProps) {
         super(scope, id, props)
 
+        const logGroup = new cwlogs.LogGroup(this, 'EcommerceApiLogs')
+
         const api = new apigateway.RestApi(this, 'EcommerceApi', {
-            restApiName: 'EcommerceApi'
+            restApiName: 'EcommerceApi',
+            deployOptions: {
+                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    caller: true,
+                    user: true
+                })
+            }
         })
+
+        const productsFetchIntegration = new apigateway.LambdaIntegration(props.productFetchHandler)
+
+        // "/products"
+        const productsResource = api.root.addResource('products')
+        productsResource.addMethod('GET', productsFetchIntegration)
     }
 }
