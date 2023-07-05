da98ec1cdabbc15343fe40e3f56c998de98d6694
Murilo Damario <murilo.gcdamario@gmail.com>
2023-07-05 20:30:57 -0300

    Publica mensagem com atributo no tópico do SNS

--- lambda/orders/ordersFunction.ts
+++ lambda/orders/ordersFunction.ts
@@ -210,6 +210,12 @@ function sendOrderEvent(order: Order, eventType: OrderEventType, lambdaRequestId
 
     return snsClient.publish({
         TopicArn: orderEventsTopicArn,
-        Message: JSON.stringify(envelope)
+        Message: JSON.stringify(envelope),
+        MessageAttributes: {
+            eventType: {
+                DataType: 'String',
+                StringValue: eventType
+            }
+        }
     }).promise()
 }
