0097bb79a76078bb54e256b3afcb0ad9c6088231
Murilo Damario <murilo.gcdamario@gmail.com>
2023-07-19 00:45:21 -0300

    Cria funções de pesquisa de eventos pelo Global Secondary Index

--- lambda/orders/layers/orderEventsRepositoryLayer/nodejs/orderEventRepository.ts
+++ lambda/orders/layers/orderEventsRepositoryLayer/nodejs/orderEventRepository.ts
@@ -30,4 +30,30 @@ export class OrderEventRepository {
             Item: orderEvent
         }).promise()
     }
+
+    async getOrderEventsByEmail(email: string) {
+        const data = await this.dynamoDbClient.query({
+            TableName: this.eventDynamoDb,
+            IndexName: 'emailIndex',
+            KeyConditionExpression: 'email = :email AND begins_with(sk, :prefix)',
+            ExpressionAttributeValues: {
+                ':email': email,
+                'prefix': 'ORDER_'
+            }
+        }).promise()
+        return data.Items as OrderEventDdb[]
+    }
+
+    async getOrderEventsByEmailAndEventType(email: string, eventType: string) {
+        const data = await this.dynamoDbClient.query({
+            TableName: this.eventDynamoDb,
+            IndexName: 'emailIndex',
+            KeyConditionExpression: 'email = :email AND begins_with(sk, :prefix)',
+            ExpressionAttributeValues: {
+                ':email': email,
+                'prefix': eventType
+            }
+        }).promise()
+        return data.Items as OrderEventDdb[]
+    }
 }
