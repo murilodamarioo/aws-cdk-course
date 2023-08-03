6645670d0325352abe932af53eb45a0958495dc8
Murilo Damario <murilo.gcdamario@gmail.com>
2023-08-03 20:41:47 -0300

    Cria Lambda Layer de notas fiscais

--- lambda/invoices/layers/invoiceRepository/nodejs/invoiceRepository.ts
+++ lambda/invoices/layers/invoiceRepository/nodejs/invoiceRepository.ts
@@ -1,4 +1,39 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+
+export interface InvoiceFile {
+    customerName: string;
+    invoiceNumber: string;
+    totalValue: number;
+    productId: string;
+    quantity: number;
+}
+
 export interface Invoice {
     pk: string;
     sk: string;
+    totalValue: number;
+    productId: string;
+    quantity: number;
+    transactionId: string;
+    ttl: number;
+    createdAt: number;
+}
+
+export class InvoiceRepository {
+    private dynamoDbClient: DocumentClient
+    private invoicesDdb: string
+
+    constructor(dynamoDbClient: DocumentClient, invoicesDdb: string) {
+        this.dynamoDbClient = dynamoDbClient
+        this.invoicesDdb = invoicesDdb
+    }
+
+    async create(invoice: Invoice): Promise<Invoice> {
+        await this.dynamoDbClient.put({
+            TableName: this.invoicesDdb,
+            Item: invoice
+        }).promise()
+
+        return invoice
+    }
 }
